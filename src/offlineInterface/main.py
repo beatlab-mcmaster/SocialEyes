5bc378788b4179c5f4a7a7613b00c3120c78d551
Shreshth Saxena <shreshth.saxena@outlook.com>
2025-11-13 18:11:08 -0500

    Fix case sensitivity in device name conversion

--- src/offlineInterface/main.py
+++ src/offlineInterface/main.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
                     
                     #Adjust offsets
                     for file_path in tqdm(file_paths, desc = stream):
-                        dname = FileProcessor.device_name_from_path(file_path, true) #Device name in offset file is always stored as an IP address.
+                        dname = FileProcessor.device_name_from_path(file_path, True) #Device name in offset file is always stored as an IP address.
                         if dname == None:
                             print("Skipping, cannot convert to device name: ", file_path)
                             continue
